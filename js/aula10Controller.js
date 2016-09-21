de202535ab9c177b3bfc83d48b7d3de99e228ec3
akemioshiro@hotmail.com <akemioshiro@hotmail.com>
2016-09-21 01:33:51 -0300

    aula 10 - filters

---
+++ js/aula10Controller.js
@@ -0,0 +1,12 @@
+﻿// outra forma de criação de controllers
+function Aula10Controller($scope, $filter) {
+    console.log("Iniciou o controller");
+    console.log($filter('retornaOi')("Patricia"));
+
+    $scope.pessoa = {
+        nome: "Devmedia",
+        idade:31
+    };
+
+}
+app.controller("Aula10Controller", Aula10Controller);
