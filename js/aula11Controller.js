2e0c0843dce2645094135f30e316311ddbad89eb
akemioshiro@hotmail.com <akemioshiro@hotmail.com>
2016-09-21 02:14:22 -0300

    aula 11 - ngRoute

---
+++ js/aula11Controller.js
@@ -0,0 +1,39 @@
+﻿angular.module("app", ["ngRoute"])
+.controller("Aula11Controller", function ($scope, $route, $routeParams, $location) {
+    $scope.$route = $route;
+    $scope.$location = $location;
+    $scope.$routeParams = $routeParams;
+})
+.controller("InicialController", function ($scope) {
+    console.log("Entrou no controller inicial");
+})
+.controller("Pagina1Controller", function ($scope) {
+    console.log("Entrou no controller pagina1");
+})
+.controller("Pagina2Controller", function ($scope, $routeParams) {
+    $scope.parametros = $routeParams;
+    console.log("Entrou no controller pagina2");
+    console.log("id do produto: " + $scope.parametros.idproduto);
+})
+
+// configurando esquema de routes
+.config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+    .when('/', {
+        templateUrl: 'aula11_inicial.html',
+        controller: 'InicialController'
+    })
+    .when('/Pagina1', {
+        templateUrl: "aula11_pagina1.html",
+        controller: "Pagina1Controller"
+    })
+    .when('/Pagina2/:idproduto', {
+        templateUrl: "aula11_pagina2.html",
+        controller: "Pagina2Controller"
+    })
+    .otherwise({
+        redirectoTo:'/'
+    });
+
+    $locationProvider.html5Mode(true);
+});
