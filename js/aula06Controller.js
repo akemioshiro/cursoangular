370f6d369ef1c16f8635730f4f211e3fa43fc166
akemioshiro@hotmail.com <akemioshiro@hotmail.com>
2016-09-20 22:41:15 -0300

    aula 06 - controllers

---
+++ js/aula06Controller.js
@@ -0,0 +1,8 @@
+﻿// nome do controller e uma function, depois injetar uma var scope (var do angular que vai ter todas var e funções)
+app.controller("aula06Controller", function ($scope) {
+    $scope.nome = "Curso Angular JS DevMedia";
+    
+    $scope.olaMundo = function (nome) {
+        alert("Olá Mundo " + nome + " !");
+    }
+});
