2bca5c687ad5f543fd1af87a3ce393e9e06794b6
akemioshiro@hotmail.com <akemioshiro@hotmail.com>
2016-09-20 23:17:42 -0300

    Aula 07 - ng-repeat

---
+++ js/aula05.appSaudacao.module.js
@@ -0,0 +1,10 @@
+﻿//declaração da aplicação, declaração de um modulo
+// dentro de [] injeta dependencias, algo que nao eh nativo do angular, pode ser um plugin
+var appSaudacao = angular.module("appSaudacao", []);
+
+// incrementando o modulo, criando um filtro
+appSaudacao.filter("ola", function () {
+    return function (nome) {
+        return "Olá " + nome + "!";
+    }
+});
---
+++ js/aula07Controller.js
@@ -0,0 +1,32 @@
+﻿app.controller("aula07Controller", function ($scope) {
+    // var do angular que concentra variaveis e funcoes, sera disponivel na camada de vsualizaçãp
+    $scope.nomes = ["Patricia", "Daniel", "Maria", "Tiago", "Marcela", "Karoline"]; 
+
+    // array de objetos
+    $scope.pessoas = [];
+    $scope.pessoas.push(
+        { nome: "Pedro", idade:31, status: false }
+    );
+    $scope.pessoas.push(
+        { nome: "Mateus", idade: 31, status: false }
+    );
+    $scope.pessoas.push(
+        { nome: "Joao", idade: 50, status: true }
+    );
+
+    console.log($scope.pessoas);
+
+    $scope.adicionaPessoas = function()
+    {
+        var nome = document.getElementById("nomepessoa");
+        var idade = document.getElementById("idadepessoa");
+
+        $scope.pessoas.push(
+                { nome: nome.value, idade: idade.value, status: true }
+        );
+
+        nome.value = "";
+        idade.value = "";
+    }
+
+});
