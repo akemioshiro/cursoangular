e52c8c6a39095b51d6c8d2995dfa18a43ea1b971
akemioshiro@hotmail.com <akemioshiro@hotmail.com>
2016-09-21 00:35:00 -0300

    aula 08 - formularios e angular

---
+++ js/aula08Controller.js
@@ -0,0 +1,29 @@
+﻿app.controller("aula08Controller", function ($scope) {
+
+    $scope.estados = ["ES","SP", "RS", "RJ", "SC", "MG"];
+
+    $scope.pessoa = novaPessoa();
+    $scope.pessoas = [];
+    
+    $scope.salvarPessoa = function (pessoa) {
+        $scope.pessoas.push(pessoa);
+        $scope.pessoa = novaPessoa();
+
+        // como se os campos nao estivessem com interação, removem a interação
+        $scope.frm.$setUntouched();
+
+        //voltando a situação original do formulario
+        $scope.frm.$setPristine();
+    };
+    
+});
+
+function novaPessoa()
+{
+    return{
+        nome: "",
+        email:"",
+        sexo: "f",
+        estado:"RJ"
+    };
+}
