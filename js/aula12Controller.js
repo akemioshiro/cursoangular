85572b370d040b41279669152ff7dcec79654ee8
akemioshiro@hotmail.com <akemioshiro@hotmail.com>
2016-10-09 06:07:14 -0300

    aula 12 - principais diretivas

---
+++ js/aula12Controller.js
@@ -0,0 +1,12 @@
+﻿// injetando dependencia ngSanitize
+var app = angular.module("app", ['ngSanitize']);
+
+// outra forma de criação de controllers
+function Aula12Controller($scope, $filter) {
+    $scope.mostrar = true;
+    $scope.variavelHtml = "<h1> Seja bem vindo ao nosso site</h1>";
+    $scope.minhaClass = "";
+    $scope.variavelCloak = "teste teste teste teste  teste teste teste teste teste";
+    $scope.minhaFoto = "foto1.jpg";
+}
+app.controller("Aula12Controller", Aula12Controller);
